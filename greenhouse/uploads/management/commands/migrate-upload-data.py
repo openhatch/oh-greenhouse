80951b7266537a75273360976262984ad51f1483
daveeloo <daveeloo@gmail.com>
2013-08-06 14:29:14 -0700

    Fixes issue #20 UDD imports take too much memory -Removed raw sql query -Changed to query in chunks filtered by time greater than a pointer and limited in size by a constant -Process is about 30MB at start and a query with a chunk size of 5000 adds approximately 25MB for a total of 55MB peak consumption -Only uploads with a time less than the current time are uploaded to prevent uploads with dates in the future from being uploaded

--- greenhouse/uploads/management/commands/migrate-upload-data.py
+++ greenhouse/uploads/management/commands/migrate-upload-data.py
@@ -1,57 +1,48 @@
 from django.core.management.base import NoArgsCommand
 from django.core.exceptions import ObjectDoesNotExist
-from uploads.common.launchpad import lp_login as lp
 from django.db import connections
-from uploads.models import Uploads
+from django.utils import timezone
+import gc
+from uploads.models import UDD, People, Uploads
 
 
 class Command(NoArgsCommand):
     help = "Migrate upload data from UDD to django managed database."
 
     def import_uploads(self):
-        cursor = connections['udd'].cursor()
-        sql = """SELECT date, distribution, source, version, changed_by_name,
-                 changed_by_email, signed_by_name, signed_by_email
-                 FROM upload_history ORDER BY date"""
-        cursor.execute(sql)
+        greatest = []
+        CHUNK_SIZE = 5000
         try:
-            t = 'timestamp'
-            latest_entry = Uploads.objects.values(t).latest(t)[t]
+            now = UDD.objects.order_by('-date').filter(
+                date__lte=timezone.now()).reverse()[0].date
+            current = latest = Uploads.objects.latest('timestamp').timestamp
         except ObjectDoesNotExist:
-            latest_entry = None
-        for row in self.row_iter(cursor):
-            if latest_entry is None or row[0] > latest_entry:
-                if row[4] == row[6] or row[5] == row[7]:
-                    spon_email = ''
-                    spon_name = ''
-                else:
-                    spon_name = row[6]
-                    spon_email = row[7]
-                uploads = Uploads(timestamp=row[0], release=row[1],
-                                  package=row[2], version=row[3],
-                                  name_changer=row[4], email_changer=row[5],
-                                  original_email_changer=row[5],
-                                  name_sponsor=spon_name,
-                                  email_sponsor=spon_email,)
-                uploads.save()
-        cursor.close()
+            current = latest = UDD.objects.order_by('date').date
+        while current < now:
+            for u in UDD.objects.order_by(
+                    '-date').filter(date__gt=current)[:CHUNK_SIZE]:
+                if u.date < now:
+                    uploads = Uploads(timestamp=u.date,
+                                      release=u.distribution,
+                                      package=u.source, version=u.version,
+                                      name_changer=u.changed_by_name,
+                                      email_changer=u.changed_by_email,
+                                      original_email_changer=
+                                      u.changed_by_email,
+                                      name_sponsor=u.signed_by_name,
+                                      email_sponsor=u.signed_by_email)
+                    uploads.save()
+                    current = u.date
+            gc.collect()
 
         # Change the email for all new uploads
         # of people who are not authoritative
         for p in People.objects.filter(authoritative=False):
             for u in Uploads.objects.filter(
-                    timestamp__gte=latest_entry).filter(
+                    timestamp__gte=latest).filter(
                     email_changer=p.original_email):
                 u.email_changer = p.email
                 u.save()
-
-    def row_iter(self, cursor, size=1000):
-        while True:
-            rows = cursor.fetchmany(size)
-            if not rows:
-                break
-            for row in rows:
-                yield row
 
     def handle_noargs(self, **options):
         self.import_uploads()
