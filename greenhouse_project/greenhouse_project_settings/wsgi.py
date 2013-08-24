27c9a83ac4da2fbb4d630e21e2264071e7f44731
daveeloo <daveeloo@gmail.com>
2013-08-23 17:24:36 -0700

    Changed hyphens to underscores for folders -changed greenhouse-project and greenhouse-project-settings -changed paths of folders referencing them

--- greenhouse_project/greenhouse_project_settings/wsgi.py
+++ greenhouse_project/greenhouse_project_settings/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greenhouse-project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greenhouse_project_settings.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import django.core.handlers.wsgi
-project_path = os.path.join(os.path.dirname(__file__), 'greenhouse-project')
+project_path = os.path.join(os.path.dirname(__file__), 'greenhouse_project')
 sys.path.insert(0, os.path.abspath(project_path))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'greenhouse-settings.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'greenhouse_project_settings.settings'
 application = django.core.handlers.wsgi.WSGIHandler()
