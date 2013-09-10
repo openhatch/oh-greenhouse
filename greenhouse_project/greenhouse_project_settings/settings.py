ee4de669f2fb13c8c787c21608973fa3d58b252c
daveeloo <daveeloo@gmail.com>
2013-09-09 19:00:16 -0700

    Changes based on feedback for Dotcloud deployment

--- greenhouse_project/greenhouse_project_settings/settings.py
+++ greenhouse_project/greenhouse_project_settings/settings.py
@@ -23,7 +23,7 @@ try:
                 'USER': 'public-udd-mirror',
                 'PASSWORD': 'public-udd-mirror',
                 'HOST': 'public-udd-mirror.xvm.mit.edu',
-                'PORT': 5432,
+                'PORT': 465,
             }
         }
         log_file_dir = '/var/log/supervisor/greenhouse.log'
@@ -47,7 +47,7 @@ except IOError:
             'USER': 'public-udd-mirror',
             'PASSWORD': 'public-udd-mirror',
             'HOST': 'public-udd-mirror.xvm.mit.edu',
-            'PORT': 5432,
+            'PORT': 465,
         }
     }
     log_file_dir = os.path.join(PROJECT_PATH, 'logs/')
