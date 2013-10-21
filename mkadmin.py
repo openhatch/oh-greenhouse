612eabfdcc8d722fadc8400c7d556b96fe8a1e9e
daveeloo <daveeloo@gmail.com>
2013-10-21 12:00:20 -0700

    Remove Dotcloud deployment files

--- mkadmin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from django.contrib.auth.models import User
-u, created = User.objects.get_or_create(username='admin')
-if created:
-    u.set_password('123456')
-    u.is_superuser = True
-    u.is_staff = True
-    u.save()
